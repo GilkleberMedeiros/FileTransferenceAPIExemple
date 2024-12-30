87f4ebdfaa53f51efb17f7eeb56470556205c137
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2024-12-30 14:29:48 -0300

    Adicionando model File.

--- FTAPI/models.py
+++ FTAPI/models.py
@@ -5,3 +5,33 @@ from django.contrib.auth.models import AbstractUser # type: ignore
 
 class User(AbstractUser): # type: ignore
     pass
+
+
+class File(models.Model):
+    filename = models.CharField(
+        name="filename",
+        max_length=250,
+    )
+    suffix = models.CharField(
+        name="suffix",
+        max_length=15,
+    )
+    file = models.FileField(
+        name="file",
+        upload_to="files/%Y/%m/%d/", 
+        max_length=250,
+    )
+    size = models.BigIntegerField(
+        name="size",
+    )
+    url = models.URLField(
+        name="url",
+    )
+    uploaded_date = models.DateField(
+        name="uploaded_date",
+        auto_now_add=True,
+    )
+    uploaded_time = models.TimeField(
+        name="uploaded_time",
+        auto_now_add=True,
+    )
