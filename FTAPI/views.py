7daed09783f92910b269269e5b2078b25054e886
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-01-13 17:45:49 -0300

    Documentando todas as views.

--- FTAPI/views.py
+++ FTAPI/views.py
@@ -9,6 +9,8 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
 
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiResponse
+
 from .models import File
 from .serializers import FileSerializer
 from .mixins import FileHandlerMixin
@@ -18,12 +20,60 @@ from home.settings import USER_FILES_ENDPOINT
 from urllib.parse import urljoin
 
 
-# TODO: Modularizar as views em funções ou métodos
-# TODO: Criar a documentação da API
-
 # Create your views here.
 class ListCreateFilesView(APIView, FileHandlerMixin):
 
+    @extend_schema(
+        summary="List all uploaded files",
+        description=(
+            "This endpoint retrieves a list of all files stored in the database. "
+            "Each file includes metadata such as the filename, suffix, size, and URL, "
+            "but excludes the actual file content."
+        ),
+        responses={
+            200: OpenApiResponse(
+                response=FileSerializer,
+                description="Success response",
+                examples=[
+                    OpenApiExample(
+                        "Success Response",
+                        value={
+                            "data": [
+                                {
+                                    "id": 1,
+                                    "filename": "example",
+                                    "suffix": ".txt",
+                                    "size": 1024,
+                                    "url": "http://localhost:8000/files/1/",
+                                    "uploaded_date": "2025-01-06",
+                                    "uploaded_time": "10:30:45"
+                                },
+                                {
+                                    "id": 2,
+                                    "filename": "photo",
+                                    "suffix": ".jpg",
+                                    "size": 2048,
+                                    "url": "http://localhost:8000/files/2/",
+                                    "uploaded_date": "2025-01-06",
+                                    "uploaded_time": "11:45:00"
+                                }
+                            ]
+                        },
+                    ),
+                ]
+            ),
+            500: OpenApiResponse(
+                response=FileSerializer,
+                description="Internal server error",
+                examples=[OpenApiExample(
+                    "Internal Server Error",
+                    value=None,
+                    response_only=True,
+                    status_codes=["500"],
+                )]
+            ),
+        },
+    )
     @method_decorator(cache_page(60))
     def get(self, request: Request) -> Response:
         try: file_qs = File.objects.all()
@@ -41,7 +91,58 @@ class ListCreateFilesView(APIView, FileHandlerMixin):
 
         return response
 
-
+    @extend_schema(
+        summary="Create a new file record.",
+        description="""This endpoint allows you to create a new file record. 
+        It accepts a JSON request containing the `filename`, `suffix`, and `file_hex` fields. 
+        The file is converted from hex data, stored in the database, and relevant metadata is returned.""",
+        request=FileSerializer,
+        responses={
+            201: OpenApiResponse(
+                response=FileSerializer,
+                description="File created successfully. Returns file details except for the file content.",
+                examples=[
+                    OpenApiExample(
+                        "Success Response",
+                        value={
+                            "id": 1,
+                            "filename": "example",
+                            "suffix": "txt",
+                            "size": 12345,
+                            "url": "http://example.com/files/1/",
+                            "uploaded_date": "2025-01-01",
+                            "uploaded_time": "12:34:56",
+                        },
+                    )
+                ],
+            ),
+            400: OpenApiResponse(
+                response=FileSerializer,
+                description="Bad request errors that can occur.",
+                examples=[
+                    OpenApiExample(
+                        "data not valid error.",
+                        value={"Error": "Data isn't valid."}
+                    ),
+                    OpenApiExample(
+                        "Failed extracting file_hex field.",
+                        value={"Error": "Failed while extracting file_hex field from request body with error {e}."},
+                    ),
+                ]
+            ),
+        },
+        examples=[
+            OpenApiExample(
+                "Request Example",
+                value={
+                    "filename": "example",
+                    "suffix": "txt",
+                    "file_hex": "AA BD 42",
+                },
+                request_only=True,
+            ),
+        ],
+    )
     def post(self, request: Request) -> Response:
         try:
             file_hex = request.data.pop("file_hex")
@@ -83,6 +184,45 @@ class ListCreateFilesView(APIView, FileHandlerMixin):
         return Response(data=file_data)
     
 class DetailDelFilesView(APIView, FileHandlerMixin):
+    @extend_schema(
+        summary="Retrieve a single file record.",
+        description="""
+        Retrive a single file record from database, 
+        including all fields from list files view and 
+        file content as hexadecimal string.
+        """,
+        responses={
+            200: OpenApiResponse(
+                response=FileSerializer,
+                description="Success full response.",
+                examples=[
+                    OpenApiExample(
+                        name="Normal example",
+                        value={
+                            "id": 1,
+                            "filename": "example",
+                            "suffix": "txt",
+                            "size": 12345,
+                            "url": "http://example.com/files/1/",
+                            "uploaded_date": "2025-01-01",
+                            "uploaded_time": "12:34:56",
+                            "file_hex": "41 4a 4c 45",
+                        },
+                    )
+                ],
+            ),
+            404: OpenApiResponse(
+                response=FileSerializer,
+                description="File not found",
+                examples=[
+                    OpenApiExample(
+                        name="File not found",
+                        value={"Error": "File with specified id {file_id} couldn't be found to be returned."},
+                    )
+                ]
+            )
+        }
+    )
     def get(self, request: Request, file_id: int) -> Response:
         # Getting from cache if exists
         url_obj = resolve(request.path_info)
@@ -109,6 +249,27 @@ class DetailDelFilesView(APIView, FileHandlerMixin):
 
         return response
 
+    @extend_schema(
+        summary="Delete a file record.",
+        description="""
+        Delete a file record from database given a file_id. Return 404 error if file 
+        record couldn't be found.
+        """,
+        responses={
+            200: OpenApiResponse(
+                description="Deletion successfully, nothing returned.",
+                response=None,
+            ),
+            404: OpenApiResponse(
+                response=FileSerializer,
+                description="File record could not be found to deletion.",
+                examples=[OpenApiExample(
+                    "Record not found",
+                    value={"Error": "File with specified id {file_id} couldn't be found to delete."},
+                )]
+            )
+        }
+    )
     def delete(self, request: Request, file_id: int) -> Response:
         try:
             file = File.objects.get(id=file_id)
