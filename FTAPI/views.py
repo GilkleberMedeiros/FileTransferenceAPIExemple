5adc500a5cf726b8b28dfa2bf34cedb3fd7d2bd1
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-02-06 15:42:45 -0300

    Fazendo o merge das branch refatorando com docs

--- FTAPI/views.py
+++ FTAPI/views.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
+
 from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiResponse
 
 from .models import File
